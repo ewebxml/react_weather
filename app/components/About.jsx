c5d570dd6196debea49bb7b0bf9529d5abcec87e
ewebxml <ewebxml@gmail.com>
2016-12-14 16:55:38 -0500

    Changed paragraph in About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,12 +2,12 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <div id="main">
+        <div>
             <h3>About</h3>
             <p>Welcome to the about page.</p>
-            <div>
-                I like women with big fucking tits.
-            </div>
+            <p>SEO Mining is the advanced analysis process of the "Knowledge Discovery in Databases"
+                process which is an interdisciplinary subfield of computer science.
+            </p>
         </div>
     )
 };
