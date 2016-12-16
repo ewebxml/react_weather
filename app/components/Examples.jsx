4fdd35c1696d40b36e6038a133c9188cd4db6d6a
ewebxml <ewebxml@gmail.com>
2016-12-15 20:19:30 -0500

    Changed Examples.jsx and Main.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 let React = require('react');
+let{Link} = require('react-router');
 
 let Examples = (props) => {
     return (
         <div>
-        <h3>Examples</h3>
-        <p> Welcome to the Examples Page.</p>
+        <h1 className="text-center">Examples</h1>
+        <p> Here are few Example locations to try out.</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Auburn Hills'>Auburn Hills, MI </Link>
+                </li>
+                <li>
+                    <Link to='/?location=Mannheim'>Mannheim, Germany </Link>
+                </li>
+            </ol>
         </div>
     )
 };
